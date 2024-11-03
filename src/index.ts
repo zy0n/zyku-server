aee6db3e933aab8c9eea8927315ade7fa59bbc22
zy0n <zy0n.barto@gmail.com>
2024-11-03 14:35:05 -0500

    use github ref

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ console.log("loaded");
 
 const testTopic = "test-topic";
 const main = async () => {
-  const zyku = await import("zyku/dist/index.js");
+  const zyku = await import("zyku");
   const mainSeed = new Uint8Array([
     215, 24, 226, 44, 127, 255, 102, 250, 110, 148, 103, 62, 66, 82, 221, 139,
     166, 170, 82, 204, 1, 100, 22, 169, 93, 50, 176, 43, 191, 16, 6, 72,
